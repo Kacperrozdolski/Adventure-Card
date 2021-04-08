99521d820d3ad9b05c358ee08d95ef9655507ece
Kacper Rozdolski <k.rpzdolski@gmail.com>
2021-04-08 13:31:37 +0200

    Comment clear.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <!-- <router-link to="/">Home</router-link>
-  <router-link to="/easy">Easy</router-link>
-  <router-link to="/medium">Medium</router-link>
-  <router-link to="/hard">Hard</router-link> -->
   <router-view />
 </template>
 
